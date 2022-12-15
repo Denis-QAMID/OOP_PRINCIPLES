ccbfcf3f477dc5354653438dc64f0a60898c7f8d
Денис Денисов <xeddyx@mail.ru>
2022-12-15 21:42:41 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.atronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = Integer.parseInt(String.valueOf(13));
+        post.birthday.month = Integer.parseInt(String.valueOf(6));
+        post.birthday.year = Integer.parseInt(String.valueOf(1999));
+    }
+}
